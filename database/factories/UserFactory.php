4b44a6ab4e3b8e3ed3ddfc34e3d17e4e718db72c
Pedro Guerrato <pedro@quicktech.no>
2017-12-12 02:31:16 -0200

    Added nickname to User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'nickname' => $faker->lastName,
         'birthdate' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'image' => $faker->imageUrl(256, 256, 'people'),
         'gender' => rand(1, 2),
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('nickname')->nullable();
             $table->date('birthdate')->nullable();
             $table->string('image')->nullable();
             $table->enum('gender', ['female','male']);
